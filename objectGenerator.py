f6ea4a452b353d5f3f6208cfc820b7c26caabd5a
audrey <mbozorgi604@gmail.com>
2022-04-14 18:16:56 +0430

    create two files and try to fill json one with python function

---
+++ objectGenerator.py
@@ -0,0 +1,38 @@
+import json
+from random import *
+import names
+
+data = {
+    "human": [
+
+    ]
+}
+
+def colorGenerator():
+    colors = ['white', 'red', 'black', 'yellow', 'brown']
+    randNumber = randint(0, 4)
+    return colors[randNumber]
+
+def generateHumanObject():
+    object = {
+        'name': '_',
+        'age': 0,
+        'color': '_',
+        'height': 50,
+    }
+
+    object['name'] = names.get_full_name()
+    object['age'] = randint(1, 100)
+    object['color'] = colorGenerator()
+    object['height'] = randint(80, 220)
+    
+    data['human'].append(object) 
+
+
+for i in range(100):
+    generateHumanObject()
+
+dataFile = open("data.json", "w")
+json.dump(data, dataFile, indent = 6)
+dataFile.close()
+
